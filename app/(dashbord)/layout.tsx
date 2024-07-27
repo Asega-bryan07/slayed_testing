86a1d29e0e5418aabfefa74d735d5b50a3c4d694
Asega-bryan07 <Almasibryan7@gmail.com>
2024-07-27 14:35:32 +0300

    Stripe & Auth

--- app/(dashbord)/layout.tsx
+++ app/(dashbord)/layout.tsx
@@ -10,8 +10,8 @@ import { ToasterProvider } from "@/lib/ToasterProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Borcelle - Admin Dashboard",
-  description: "Admin dashboard to manage Borcelle's data",
+  title: "SLAYED BY STACY",
+  description: "Admin dashboard Account",
 };
 
 export default function RootLayout({
